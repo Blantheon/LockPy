3b21caa6b92fc541ea0276dacb3088d902cd8a2f
Blantheon <blantheon@proton.me>
2025-03-18 18:39:16 +0100

    Add a function for create a password based on a minimal entropy

--- entropy.py
+++ entropy.py
@@ -1,29 +1,39 @@
-from string import ascii_lowercase, ascii_uppercase
+from string import printable
 from math import log2
+from typing import Tuple
+from random import choice
 
-def calculate_entropy_string(password: str) -> int:
-    # calculate range of a given password before calculate entropy
-    VALUES = {'lower': 26, 'upper': 26, 'int': 10, 'special' : 32}
-    check = {'lower': True, 'upper': True, 'int': True, 'special': True}
-    res = 0
-    password_set = set(password)
-    for char in password_set:
-        if char.isalpha():
-            if char.islower() and check['lower']:
-                res += VALUES['lower']
-                check['lower'] = False
-            if char.isupper() and check['upper']:
-                res += VALUES['upper']
-                check['upper'] = False
+def calculate_entropy_string(password: str, range_password=0) -> int:
+    if not range_password:
+        # calculate range_password of a given password before calculate entropy
+        VALUES = {'lower': 26, 'upper': 26, 'int': 10, 'special' : 32}
+        check = {'lower': True, 'upper': True, 'int': True, 'special': True}
         
-        elif char.isnumeric():
-            if check['int']:
-                res += VALUES['int']
-                check['int'] = False
+        
+        password_set = set(password)
+        for char in password_set:
+            if char.isalpha():
+                if char.islower() and check['lower']:
+                    range_password += VALUES['lower']
+                    check['lower'] = False
+                if char.isupper() and check['upper']:
+                    range_password += VALUES['upper']
+                    check['upper'] = False
             
-        elif check['special']:
-            res += VALUES['special']
-            check['special'] = False
+            elif char.isnumeric():
+                if check['int']:
+                    range_password += VALUES['int']
+                    check['int'] = False
+                
+            elif check['special']:
+                range_password += VALUES['special']
+                check['special'] = False
     
     # expression for calculate entropy
-    return len(password) * log2(res)
+    return len(password) * log2(range_password)
+
+
+def create_password_string(entropy: int) -> Tuple[str, int]:
+    ENTROPY_BY_CHAR = 6.554588851677638
+    return (p := ''.join(choice(printable) for _ in range(round(entropy / ENTROPY_BY_CHAR) + 1))), calculate_entropy_string(p, 94)
+
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import argparse
 
+# resctrict number of arguments: https://stackoverflow.com/questions/13310047/how-do-i-constrain-my-python-script-to-only-accepting-one-argument-argparse
+
 parser = argparse.ArgumentParser(usage="%(prog)s [options]")
 parser.add_argument('-f', '--filename', help='Absolute path | File for choose you\'r diceware dictionnary')
 parser.add_argument('-et', '--entropy', type=int, help='Int | a minimal value of the entropy for the password')
+parser.add_argument('-c', '--calculate', help='You\'r password | calculate the entrhopy of you\'r password')
 parser.add_argument('-v', '--verbose', action=argparse.BooleanOptionalAction, default=False)
 parser.add_argument('--version', action='version', version='Pswd Calculator 1.0')
 args = parser.parse_args()
