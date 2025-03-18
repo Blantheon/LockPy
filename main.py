4a734a497cbc6c4f5cd8bba5d8ca7f83cd50825a
Blantheon <blantheon@proton.me>
2025-03-18 18:49:19 +0100

    Update main.py
    Correct syntax

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 parser = argparse.ArgumentParser(usage="%(prog)s [options]")
 parser.add_argument('-f', '--filename', help='Absolute path | File for choose you\'r diceware dictionnary')
 parser.add_argument('-et', '--entropy', type=int, help='Int | a minimal value of the entropy for the password')
-parser.add_argument('-c', '--calculate', help='You\'r password | calculate the entrhopy of you\'r password')
+parser.add_argument('-c', '--calculate', help='You\'r password | calculate the entropy of you\'r password')
 parser.add_argument('-v', '--verbose', action=argparse.BooleanOptionalAction, default=False)
 parser.add_argument('--version', action='version', version='Pswd Calculator 1.0')
 args = parser.parse_args()
