faca933705702778ab08cbf9e38d6663f2929942
Blantheon <blantheon@proton.me>
2025-04-04 17:31:28 +0200

    continue working on haveibeenpawnd module

--- modules/check_modules/ibeenpwned.py
+++ modules/check_modules/ibeenpwned.py
@@ -5,4 +5,14 @@ from typing import Tuple
 
 
 def check_password_pawned(password: str) -> Tuple[bool, int]:
-    pass
+    encoded_password = password.encode()
+    hashed = hashlib.sha1(encoded_password).hexdigest()
+    start, last = hashed[0:5], hashed[5:]
+
+    res = requests.get(f'https://api.pwnedpasswords.com/range/{}')
+
+
+
+
+
+check_password_pawned('password')
