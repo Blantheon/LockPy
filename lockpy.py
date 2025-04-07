41fd72dbebe056ff5fd75c24124bc970092356ef
Blantheon <blantheon@proton.me>
2025-04-07 11:22:50 +0200

    Continue working on

--- lockpy.py
+++ lockpy.py
@@ -5,6 +5,8 @@ from getpass import getuser
 import os
 from typing import Tuple
 import modules.create_modules.password_creators as password_mod
+import modules.check_modules.entropy as entropy
+import modules.check_modules.ibeenpwned as pawned
 
 
 def check_diceware(arguments: list[str]) -> Tuple[int, str]:
@@ -32,7 +34,7 @@ def check_diceware(arguments: list[str]) -> Tuple[int, str]:
 def parser(arguments: list[str]):
     parser = argparse.ArgumentParser(usage="%(prog)s [options]")
     parser.add_argument('--version', action='version', version='LockPy 1.0')
-    subparsers = parser.add_subparsers(required=True)
+    subparsers = parser.add_subparsers(required=True, dest='command')
     
     parser_create = subparsers.add_parser('create', help='create a new password, see: python3 lockpy.py create -h')
     parser_create = parser_create.add_mutually_exclusive_group()
@@ -51,19 +53,23 @@ def parser(arguments: list[str]):
     # -----------------Note----------------------
     # continue the big if down for check options
 
-    if not args.string and not args.diceware:
-        raise ValueError(f'A flag with an entropy greater than 0 should be selected when the subcommand create is used, see: python3 main.py create -h')
-    elif args.diceware:
-        return {'dice': check_diceware(args.diceware)}
-    else:
-        return {'str': args.string}
+    if args.command == 'create':
+        if not args.string and not args.diceware:
+            raise ValueError(f'A flag with an entropy greater than 0 should be selected when the subcommand create is used, see: python3 main.py create -h')
+        elif args.diceware:
+            return {'dice': check_diceware(args.diceware)}
+        else:
+            return {'str': args.string}
     
-
+    if args.command == 'check':
+        if args.calculate:
+            return {'calculate': args.calculate}
+        else:
+            return {'pawn': args.pawn}
+    
 
 if __name__ == '__main__':
-    # options = {'str': int(entropy)} or {'dict': (int(entropy), str(path to list))}
     options = parser(sys.argv[1:])
-    
 
     # This big tree of if will disappear and become a class in few times
     if options.get('str'):
@@ -71,8 +77,26 @@ if __name__ == '__main__':
         if int(entropy_user) < 0:
             raise ValueError('Negative entropy is not allowed')
         password = password_mod.create_password_string(entropy_user)
+    
     if options.get('dice'):
         entropy_user, list_path = options['dice']
         password = password_mod.create_password_diceware(entropy_user, list_path)
-    
+
     print(f'The new password with an entropy of {password[1]} is:\n{repr(password[0])}')
+    
+    
+    if options.get('calculate'):
+        password = options['calculate']
+        nb_spaces = password.count(' ')
+        if nb_spaces <= 2:
+            print('The password is treated as a string password')
+            entropy_pass = entropy.calculate_entropy_string(password)
+        else:
+            print('The password is treated like a diceware password')
+            entropy_pass = entropy.calculate_entropy_diceware(password)
+        print(f'The password {repr(password)} have an entropy of {entropy_pass}')
+
+    if options.get('pawn'):
+        password = options['pawn']
+        message = pawned.check_password_pawned(password)
+        print(message)
