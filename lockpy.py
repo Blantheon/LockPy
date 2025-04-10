6f354d457ea8d8ce81731bde7485adae0a069866
Blantheon <blantheon@proton.me>
2025-04-10 18:12:02 +0200

    classes finished

--- lockpy.py
+++ lockpy.py
@@ -9,6 +9,40 @@ import modules.check_modules.entropy as entropy
 import modules.check_modules.ibeenpwned as pawned
 
 
+# These class are used to do verification on user input and call function after that
+class NewPassword():
+
+    @staticmethod
+    def create_password_str(entropy_user):
+        if int(entropy_user) < 0:
+            raise ValueError('Negative entropy is not allowed')
+        return password_mod.create_password_string(entropy_user)
+
+
+    @staticmethod
+    def create_password_dice(entropy_path):
+        entropy_user, list_path = entropy_path
+        return password_mod.create_password_diceware(entropy_user, list_path)
+
+
+class CheckMethods():
+    
+    @staticmethod
+    def calculate_password_entropy(password):
+        nb_spaces = password.count(' ')
+        if nb_spaces <= 2:
+            print('The password is treated as a string password')
+            return entropy.calculate_entropy_string(password)
+        else:
+            print('The password is treated like a diceware password')
+            return entropy.calculate_entropy_diceware(password)
+    
+
+    @staticmethod
+    def check_pawned(self, password):
+        return pawned.check_password_pawned(password)
+
+
 def check_diceware(arguments: list[str]) -> Tuple[int, str]:
     if len(arguments) > 2:
         raise ValueError(f'The diceware flag take only one or two arguments in the following: int, str. See: python3 lockpy.py create -h')
@@ -38,14 +72,19 @@ def parser(arguments: list[str]):
     
     parser_create = subparsers.add_parser('create', help='create a new password, see: python3 lockpy.py create -h')
     parser_create = parser_create.add_mutually_exclusive_group()
-    parser_create.add_argument('-s', '--string', type=int, metavar='Int',help='the minimal entropy for the string password ')
-    parser_create.add_argument('-d', '--diceware', nargs='+', metavar='Int, str', help='the minimal entropy for the diceware password | OPTIONAL: a second argument with the path to the list')
+    parser_create.add_argument('-s', '--string', type=int, metavar='Int',
+                               help='the minimal entropy for the string password ')
+    parser_create.add_argument('-d', '--diceware', nargs='+', metavar='Int, str', 
+                               help='the minimal entropy for the diceware password | OPTIONAL: a second argument with the path to the list')
 
     
-    parser_check = subparsers.add_parser('check', help="check the efficacity of you'r password, see python3 lockpy.py check -h")
+    parser_check = subparsers.add_parser('check', 
+                                         help="check the efficacity of you'r password, see python3 lockpy.py check -h")
     parser_check = parser_check.add_mutually_exclusive_group()
-    parser_check.add_argument('-c', '--calculate',type=str, metavar='Str', help='calculate entropy of a password')
-    parser_check.add_argument('-p', '--pawn', type=str, metavar='Str', help='check if a password has leaked on haveibeenpawned')
+    parser_check.add_argument('-c', '--calculate',type=str, metavar='Str', 
+                              help='calculate entropy of a password')
+    parser_check.add_argument('-p', '--pawn', type=str, metavar='Str',
+                              help='check if a password has leaked on haveibeenpawned')
     
     
     args = parser.parse_args(arguments)
@@ -58,52 +97,29 @@ def parser(arguments: list[str]):
         if not args.string and not args.diceware:
             raise ValueError(f'A flag with an entropy greater than 0 should be selected when the subcommand create is used, see: python3 lockpy.py create -h')
         elif args.diceware:
-            return {'dice': check_diceware(args.diceware)}
+            return ('dice', check_diceware(args.diceware))
         else:
-            return {'str': args.string}
+            return ('str', args.string)
     
     if args.command == 'check':
         if args.calculate:
-            return {'calculate': args.calculate}
+            return ('calculate', args.calculate)
         elif args.pawn:
-            return {'pawn': args.pawn}
+            return ('pawn', args.pawn)
         else:
             raise ValueError('A flag should be selected when the subcommand check is used, see: python3 lockpy.py create -h')
     
 
 def main():
-    options = parser(sys.argv[1:])
+    options = parser(['create', '-d', '59'])#sys.argv[1:])
 
-    # This big tree of if will disappear and become a class in few times
-    if options.get('str') or options.get('dice'):
-        if options.get('str'):
-            entropy_user = options['str']
-            if int(entropy_user) < 0:
-                raise ValueError('Negative entropy is not allowed')
-            password = password_mod.create_password_string(entropy_user)
-        
-        else:
-            entropy_user, list_path = options['dice']
-            password = password_mod.create_password_diceware(entropy_user, list_path)
-
-        print(f'The new password with an entropy of {password[1]} is:\n{repr(password[0])}')
+    entropy_or_path = options[1]
+    methods = {'str': NewPassword.create_password_str,
+               'dice': NewPassword.create_password_dice,
+               'calculate': CheckMethods.calculate_password_entropy,
+               'pawn': CheckMethods.check_pawned}
     
-    
-    if options.get('calculate'):
-        password = options['calculate']
-        nb_spaces = password.count(' ')
-        if nb_spaces <= 2:
-            print('The password is treated as a string password')
-            entropy_pass = entropy.calculate_entropy_string(password)
-        else:
-            print('The password is treated like a diceware password')
-            entropy_pass = entropy.calculate_entropy_diceware(password)
-        print(f'The password {repr(password)} have an entropy of {entropy_pass}')
-
-    if options.get('pawn'):
-        password = options['pawn']
-        message = pawned.check_password_pawned(password)
-        print(message)
+    print(methods[options[0]](entropy_or_path))
 
 
 if __name__ == '__main__':
