0d738314e5f81313858aa83940321ac515185ade
Blantheon <blantheon@proton.me>
2025-04-13 12:55:38 +0200

    Test sql db

--- sql/sql.py
+++ sql/sql.py
@@ -8,11 +8,18 @@ class Database():
 
     def connect(self):
         self.con = sqlite3.connect(self.name)
-        self.cursor = self.con.cursor().execute()
+        self.cursor = self.con.cursor()
 
 
 
 
 db = Database('test.db')
 db.connect()
-db.cursor()
+sql = '''CREATE TABLE emp ( 
+staff_number INTEGER PRIMARY KEY, 
+fname VARCHAR(20), 
+lname VARCHAR(30), 
+gender CHAR(1), 
+joining DATE);'''
+db.cursor.execute(sql)
+db.con.close()
