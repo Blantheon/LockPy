a66c3ce82d4b9e043c508c2dd66f8590f709fe21
Blantheon <blantheon@proton.me>
2025-04-27 19:53:06 +0200

    Continue working

--- sql/encrypt.py
+++ sql/encrypt.py
@@ -1,22 +1,30 @@
 from cryptography.fernet import Fernet
 from getpass import getuser
-
-
+import hashlib
+from os import urandom
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 class Encryption():
 
     def __init__(self, key=None) -> None:
         if not key:
-            self.generate_key()
+            self.new_key()
 
         self.f = Fernet(self.key)
 
-    def generate_key(self):
+    def new_key(self, password: str = None):
         self.key = Fernet.generate_key()
-        # The key is written in clear
-        # TO ADD: write a hash of the key
-        with open('/home/' + getuser() + '/Desktop/lockpy/sql/.password.key') as f:
-            f.write(self.key)
+        if not password:
+            password = input('Enter a password for the database: ')
+
+        # TO ADD: write a crypted key
+        salt: bytes = urandom(16)
+        kdf = PBKDF2HMAC(hashes.SHA256(), 32, salt, 1_000_000)
+        print(kdf.derive(b'password'))
+        
+        '''with open('/home/' + getuser() + '/Desktop/lockpy/sql/.password.key') as f:
+            f.write(self.key)'''
 
     
     def encrypt(self, data: bytes | str) -> None:
@@ -28,3 +36,6 @@ class Encryption():
 
     def decrypt(self, data: bytes) -> None:
         return self.f.decrypt(data)
+
+
+e = Encryption()
