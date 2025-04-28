bae1f7d05787721c34ae2daafd209e3182abe7a7
Blantheon <blantheon@proton.me>
2025-04-28 18:15:45 +0200

    Continue working on encryption

--- sql/encrypt.py
+++ sql/encrypt.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
 from getpass import getuser
-import hashlib
+from base64 import urlsafe_b64encode
 from os import urandom
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -16,21 +16,32 @@ class Encryption():
 
     def new_key(self, password: str = None):
         self.key: bytes = Fernet.generate_key()
-        if not password:
-            password = input('Enter a password for the database: ')
-
-        # TO ADD: encrypt the key with AES-GCM with a derived key by PBKDF2 based on the user password
-        salt: bytes = urandom(16)
-        kdf = PBKDF2HMAC(hashes.SHA256(), 32, salt, 1_000_000)
-        key: bytes = kdf.derive(b'password')
+        pbkdf = self.pbkdf_derivation(password)
+        f = Fernet(urlsafe_b64encode(pbkdf))
+        print(f)
+        
+        
         
         '''with open('/home/' + getuser() + '/Desktop/lockpy/sql/.password.key') as f:
             f.write(self.key)'''
 
+
+    def pbkdf_derivation(self, password: str | None) -> bytes:
+        if not password:
+            password = input('Enter a password for the database: ')
+
+        password = password.encode()
+        salt: bytes = urandom(16)
+        kdf = PBKDF2HMAC(hashes.SHA256(), 32, salt, 1_000_000)
+        key: bytes = kdf.derive(password)
+        
+        with open('/home/' + getuser() + '/Desktop/lockpy/sql/.salt.bin', 'w') as f:
+            f.write(salt)
+        return key
     
     def encrypt(self, data: bytes | str) -> None:
         if isinstance(data, str):
-            data = bytes(data)
+            data = data.encode()
 
         return self.f.encrypt(data)
     
