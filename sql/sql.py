874bef706ccdba37a6285ba387a4eeb33daee973
Blantheon <blantheon@proton.me>
2025-05-03 17:40:56 +0200

    Bug fixes in encryption and new sql module finished

--- sql/sql.py
+++ sql/sql.py
@@ -1,73 +1,96 @@
-import sqlite3
 import apsw
-from typing import Generator
 from getpass import getuser
+import os
+from io import BytesIO
+import sys
+from encrypt import Cryptography, VFSRamOnly
 
 PATH = '/home/' + getuser() + '/Desktop/lockpy/sql/'
 
 class Database():
     def __init__(self, path: str) -> None:
         self.path = PATH + path
+        self.crypto = Cryptography()
+        self.vfs: VFSRamOnly
+        self.con : apsw.Connection
+        self.state: str
+        self.db_bytes: BytesIO
         self.connect()
-
+    
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # encrypt the database
+        self.vfs.files.buffer.seek(0)
+        self.crypto.encrypt(self.vfs.files.buffer.read())
         self.con.close()
+        os.remove(PATH + '.Nothing')
 
-    def connect(self) -> None:
-        self.con = sqlite3.connect(self.path)
-        self.cursor = self.con.cursor()
-        # check if the table exist
-        sql_command = "SELECT name FROM sqlite_master WHERE type='table' AND name='password';"
-        self.cursor.execute(sql_command)
-        response = self.cursor.fetchall()
-        if not response:
-            self.create_table()
 
-    def create_table(self) -> None:
-        sql_command = f'''CREATE TABLE password(\
+
+    def connect(self):
+        if os.path.isfile(self.path):
+            # if the db exist then it's encrypted
+            with open(self.path, 'rb') as f:
+                decrypted_data = self.crypto.decrypt(f.read())
+            self.db_bytes = BytesIO(decrypted_data)
+        else:
+            # just to create the database with good headers/format
+            con = apsw.Connection(self.path)
+            con.execute('CREATE TABLE empty(name TEXT PRIMARY KEY NOT NULL);')
+            con.execute('DROP TABLE empty;')
+            del con
+            with open(self.path, 'rb') as f:
+                self.db_bytes = BytesIO(f.read())
+        self.vfs = VFSRamOnly(self.db_bytes)
+        self.con = apsw.Connection('.Nothing', vfs=self.vfs.vfs_name)
+        self.create_table()
+
+    def create_table(self):
+        tables = [i for i in self.con.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='password';")]
+        if not tables:
+            sql_command = f'''CREATE TABLE password(\
                         name TEXT PRIMARY KEY NOT NULL,\
                         user TEXT,\
                         password TEXT NOT NULL,\
                         url TEXT,\
                         description TEXT);'''
-        self.cursor.execute(sql_command)
+            self.con.execute(sql_command)
 
     def add_in_db(self, values: str) -> None:
         sql_command = f'INSERT INTO password VALUES {values};'
-        self.cursor.execute(sql_command)
-        self.con.commit()        
+        self.con.execute(sql_command)
 
-    def select_in_db(self, name: str) -> None:
+    def select_in_db(self, name:str) -> None:
         sql_command = f'SELECT * FROM password'
         if name != 'all':
             sql_command +=  f' WHERE name="{name}"'
-        self.cursor.execute(sql_command)
-        lines = self.cursor.fetchall()
+        lines = [i for i in self.con.execute(sql_command)]
         if not lines:
             raise ValueError('The service entered doesn\'t exist')
         for word in lines:
-            d = {'Service': word[0], 'User': word[1], 'Password': word[2], 'Url': word[3], 'Description': word[4]}
+            d = {'Service': word[0], 'User': word[1], 'Passline': word[2], 'Url': word[3], 'Description': word[4]}
             print('-' * 28)
             print('\n'.join(f'{k}: {d[k]}' for k in d if d[k]))
 
-    def update_db(self, values: dict[str]) -> None:
+    def update_db(self, values:dict[str: str]) -> None:
         name, column, new_value = values['name'], values['column'], values['value']
         sql_command = f'UPDATE password SET {column}="{new_value}" WHERE name="{name}";'
-        self.cursor.execute(sql_command)
-        self.con.commit()
+        self.con.execute(sql_command)
     
     def delete_in_db(self, name: str) -> None:
         sql_command = f'DELETE FROM password WHERE name="{name}";'
-        self.cursor.execute(sql_command)
-        self.con.commit()
+        self.con.execute(sql_command)
 
 
 if __name__ == '__main__':
-    with Database('test.lp') as db:
-        db.add_in_db()
+    with Database('database.lp') as db:
+        db.add_in_db('("YoBro","Myman", "A Good Pass", "https://gge.com", "A Big ldfsjfldsv Description")')
+        db.select_in_db('all')
+    
+        
+
 
 """
 # SQL query to view different table
